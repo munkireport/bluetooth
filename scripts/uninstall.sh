88ed818280a9b087ecf2181d6182ff479490bc3d
Arjen van Bochoven <nejra@me.com>
2019-09-24 23:26:56 +0200

    Fix uninstaller for bluetooth

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Remove bluetooth script
-rm -f "${MUNKIPATH}preflight.d/bluetooth.{sh,py}"
+rm -f "${MUNKIPATH}preflight.d/bluetooth.py"
+rm -f "${MUNKIPATH}preflight.d/bluetooth.sh"
 
 # Remove bluetoothinfo.txt file
-rm -f "${CACHEPATH}bluetoothinfo.{txt,plist}"
+rm -f "${CACHEPATH}bluetoothinfo.txt"
+rm -f "${CACHEPATH}bluetoothinfo.plist"
