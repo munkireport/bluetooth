58c04d68fb8c627519e1f846e7be20ad11227ee9
bochoven <arjen@bochoven.net>
2020-04-17 11:44:51 +0200

    Convert to new model

--- bluetooth_controller.php
+++ bluetooth_controller.php
@@ -34,15 +34,12 @@ class Bluetooth_controller extends Module_controller
     **/
     public function get_power_state()
     {
-        $obj = new View();
-        $queryobj = new Ms_office_model();
-        $sql = "SELECT COUNT(CASE WHEN `power` = '1' THEN 1 END) AS 'on',
-                        COUNT(CASE WHEN `power` = '0' THEN 0 END) AS 'off'
-                        from bluetooth
-                        LEFT JOIN reportdata USING (serial_number)
-                        WHERE
-                            ".get_machine_group_filter('');
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+        jsonView(
+            Bluetooth_model::selectRaw("COUNT(CASE WHEN `power` = '1' THEN 1 END) AS 'on'")
+                ->selectRaw("COUNT(CASE WHEN `power` = '0' THEN 1 END) AS 'off'")
+                ->filter()
+                ->first()
+        );
     }
 
      /**
@@ -51,19 +48,14 @@ class Bluetooth_controller extends Module_controller
      *
      **/
      public function bluetooth_low_battery()
-     {        
-        $obj = new View();
-        $queryobj = new Bluetooth_model();
-
-        $sql = "SELECT device_address, batterypercent, device_name, serial_number
-						FROM bluetooth
-						LEFT JOIN reportdata USING (serial_number)
-						WHERE (`batterypercent` <= '15')
-						".get_machine_group_filter('AND');
-
-        $bluetooth_array = $queryobj->query($sql);
-        $obj->view('json', array('msg' => current(array('msg' => $bluetooth_array)))); 
-     }
+     {
+        jsonView(
+            Bluetooth_model::select()
+                ->where('batterypercent', '<=', 15)
+                ->filter()
+                ->get()
+        );
+   }
 
 	/**
      * Retrieve data in json format
@@ -71,16 +63,11 @@ class Bluetooth_controller extends Module_controller
      **/
     public function get_bluetooth_data($serial_number)
     {
-        $obj = new View();
-        $queryobj = new Bluetooth_model();
-        
-        $sql = "SELECT machine_address, power, discoverable, supports_lowenergy, supports_airdrop, supports_instanthotspot, supports_handoff, remotewake, autoseek_keyboard, autoseek_pointing, connectable, vendor_id, apple_bluetooth_version, device_name, device_address, isconnected, ispaired, isconfigured, isnormallyconnectable, rssi, batterypercent, manufacturer, majorclass, minorclass, services
-                        FROM bluetooth
-                        WHERE serial_number = '$serial_number'";
-        
-        $bluetooth_tab = $queryobj->query($sql);
-
-        $bluetooth = new Bluetooth_model;
-        $obj->view('json', array('msg' => current(array('msg' => $bluetooth_tab)))); 
-    }
+        jsonView(
+            Bluetooth_model::select()
+                ->where('bluetooth.serial_number', $serial_number)
+                ->filter()
+                ->get()
+        );
+   }
 } // End class Bluetooth_controller
--- bluetooth_model.php
+++ bluetooth_model.php
@@ -1,92 +1,41 @@
 <?php
 
-use CFPropertyList\CFPropertyList;
+use munkireport\models\MRModel as Eloquent;
 
-class Bluetooth_model extends \Model {
+class Bluetooth_model extends Eloquent
+{
+    protected $table = 'bluetooth';
 
-	function __construct($serial='')
-	{
-		parent::__construct('id', 'bluetooth'); //primary key, tablename
-		$this->rs['id'] = '';
-		$this->rs['serial_number'] = $serial;
-		$this->rs['machine_address'] = null;
-		$this->rs['autoseek_keyboard'] = null; // true/false
-		$this->rs['autoseek_pointing'] = null; // true/false
-		$this->rs['connectable'] = null; // true/false
-		$this->rs['discoverable'] = null; // true/false
-		$this->rs['hardware_transport'] = null;
-		$this->rs['machine_name'] = null;
-		$this->rs['power'] = null;
-		$this->rs['remotewake'] = null;
-		$this->rs['supports_handoff'] = null;  // true/false
-		$this->rs['supports_instanthotspot'] = null; // true/false
-		$this->rs['supports_airdrop'] = null; // true/false
-		$this->rs['supports_lowenergy'] = null; // true/false
-		$this->rs['vendor_id'] = null;
-		$this->rs['apple_bluetooth_version'] = null;
-		$this->rs['device_name'] = null;
-		$this->rs['device_address'] = null;
-		$this->rs['rssi'] = null;
-		$this->rs['batterypercent'] = null;
-		$this->rs['isnormallyconnectable'] = null; // true/false
-		$this->rs['isconfigured'] = null; // true/false
-		$this->rs['isconnected'] = null; // true/false
-		$this->rs['ispaired'] = null; // true/false
-		$this->rs['manufacturer'] = null;
-		$this->rs['majorclass'] = null;
-		$this->rs['minorclass'] = null;
-		$this->rs['services'] = null;
-        
-		$this->serial_number = $serial;
-	}
-	
-	// ------------------------------------------------------------------------
-    
-	/**
-	 * Process data sent by postflight
-	 *
-	 * @param string data
-	 * @author tuxudo
-	 **/
-	function process($plist)
-	{
-        // Check if we have data
-		if ( ! $plist){
-			throw new Exception("Error Processing Request: No property list found", 1);
-		}
+    protected $fillable = [
+		'serial_number',
+		'machine_address',
+		'autoseek_keyboard',
+		'autoseek_pointing',
+		'connectable',
+		'discoverable',
+		'hardware_transport',
+		'machine_name',
+		'power',
+		'remotewake',
+		'supports_handoff',
+		'supports_instanthotspot',
+		'supports_airdrop',
+		'supports_lowenergy',
+		'vendor_id',
+		'apple_bluetooth_version',
+		'device_name',
+		'device_address',
+		'rssi',
+		'batterypercent',
+		'isnormallyconnectable',
+		'isconfigured',
+		'isconnected',
+		'ispaired',
+		'manufacturer',
+		'majorclass',
+		'minorclass',
+		'services',
+    ];
 
-		// Delete previous set
-        $this->deleteWhere('serial_number=?', $this->serial_number);
-
-		$parser = new CFPropertyList();
-		$parser->parse($plist, CFPropertyList::FORMAT_XML);
-		$bt_list = $parser->toArray();
-
-        // Process each Bluetooth device
-        foreach ($bt_list as $btdevice) {
-
-            // Process singular bluetooth data
-            $singularitems = array ('machine_address','autoseek_keyboard','autoseek_pointing','connectable','discoverable','hardware_transport','machine_name','power','remotewake','supports_handoff','supports_instanthotspot','supports_airdrop','supports_lowenergy','vendor_id','apple_bluetooth_version');
-            foreach ($singularitems as $singlekey) {
-                if( array_key_exists($singlekey, $btdevice)){
-                    $this->rs[$singlekey] = $btdevice[$singlekey]; 
-                } else {
-                    $this->rs[$singlekey] = null;
-                }
-            }
-            
-            // Process device data
-			foreach ($this->rs as $key => $value) {
-				$this->rs[$key] = $value;
-				if(array_key_exists($key, $btdevice))
-				{
-					$this->rs[$key] = $btdevice[$key];
-				}
-			}
-
-			// Save Bluetooth device data, I've traveled each and every highway, And more, much more than this
-			$this->id = '';
-			$this->save();
-		}
-	}
+    public $timestamps = false;
 }
